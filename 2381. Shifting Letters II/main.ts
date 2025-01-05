9a4f58961f6a56d090d9c98a1cbe40f3fbe731b8
shohjahon1code <suyunovshohjahon08@mail.ru>
2025-01-05 19:09:02 +0500

    add shifting letter ii puzzle

--- 2381. Shifting Letters II/main.ts
+++ 2381. Shifting Letters II/main.ts
@@ -1,4 +1,35 @@
 function shiftingLetters(s: string, shifts: number[][]): string {
-  
-    return 's'
+    let n = s.length
+    let difArr = new Array(n + 1).fill(0)
+
+    for (const [start, end, direction] of shifts) {
+        if (direction === 1) {
+            difArr[start] += 1;     
+            difArr[end + 1] -= 1;   
+        } else {
+            difArr[start] -= 1;     
+            difArr[end + 1] += 1;
+        }
+    }
+
+    let prefixSum = 0;
+    let result = '';
+
+
+    for (let i = 0; i < n; i++) {
+        prefixSum += difArr[i]
+
+        let code = s.charCodeAt(i) - 'a'.charCodeAt(0);
+        console.log(code);
+        
+        let newCode = ((code + prefixSum) % 26 + 26) % 26;
+
+        result += String.fromCharCode(newCode + 'a'.charCodeAt(0));
+    }
+
+    return result
 };
+
+let s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]]
+
+console.log(shiftingLetters(s, shifts));

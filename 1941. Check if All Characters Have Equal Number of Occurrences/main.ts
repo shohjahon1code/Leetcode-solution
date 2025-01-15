5bd3e9af4dda4e3eae21c100c4652a4fb5d64fb4
shohjahon1code <suyunovshohjahon08@mail.ru>
2025-01-15 21:31:25 +0500

    added new puzzle

---
+++ 1941. Check if All Characters Have Equal Number of Occurrences/main.ts
@@ -0,0 +1,18 @@
+function areOccurrencesEqual(s: string): boolean {
+    let frequencyObj: Record<string, number> = {}
+
+    for (let char of s) {
+        if (!frequencyObj[char]) {
+            frequencyObj[char] = 1
+        } else {
+            frequencyObj[char] += 1
+        }
+    }
+
+    let frequency = Object.values(frequencyObj)
+
+    return frequency.every((freq: number) => freq === frequency[0]);
+};
+
+let s = "abacbc"
+console.log(areOccurrencesEqual(s));
---
+++ 2429. Minimize XOR/main.ts
@@ -0,0 +1,11 @@
+function minimizeXor(num1: number, num2: number): number {
+    
+    console.log(num1.toString(2));
+    
+
+    return (num1 - num2)
+};
+
+let num1 = 3, num2 = 5
+
+console.log(minimizeXor(num1, num2));

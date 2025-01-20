0d0543b40ba1a7a3ec6a59c3af628e988d8871e7
shohjahon1code <suyunovshohjahon08@mail.ru>
2025-01-20 22:07:54 +0500

    f

---
+++ 3340. Check Balanced String/main.ts
@@ -0,0 +1,21 @@
+function isBalanced(num: string): boolean {
+    let even = 0
+    let odd = 0
+
+    for (let i = 0; i < num.length; i++) {
+        let digit = parseInt(num[i], 10)
+
+        if (i % 2 == 0) {
+            even += digit
+        }else{
+            odd += digit
+        }
+    }
+
+    return even === odd
+};
+
+
+let num = "24123"
+
+console.log(isBalanced(num));
